c6d7182ff278641ad15911bfbfb09f35c55a0898
tanishatasin <tasnintanisha@gmail.com>
2022-11-16 02:16:37 +0600

    add

---
+++ 1.c
@@ -0,0 +1,13 @@
+//a) Declare a variable uninitialized
+#include<stdio.h>
+int main()
+{
+   int x ;   //Declare a variable uninitialized
+
+   int y =12 ;  //Declare and initialize a variable in one statement
+
+   int a=c=d=e= 0;  //Declare and initialize multiple variables with the same value in one statement
+    printf("a = %d, c = %d, d = %d, e = %d", a, c, d, e);
+
+
+}
